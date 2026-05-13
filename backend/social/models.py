64abcea06f32dbe92708f589a5dc009222cf2852
diegonuunez <diegonunco12@gmail.com>
2026-05-13 18:34:59 +0200

    Add Social Models

--- backend/social/models.py
+++ backend/social/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from missions.models import Mission
 
-# Create your models here.
+User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='given_likes')
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'mission') 
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.mission.name}"
+
+class Save(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='saved_missions_list')
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE, related_name='saved_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'mission')
+
+    def __str__(self):
+        return f"{self.user.username} saved {self.mission.name}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    is_edited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.mission.name}"
