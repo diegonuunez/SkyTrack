95d63d9e2715dcea0a47de6c068670e00a3d3903
diegonuunez <diegonunco12@gmail.com>
2026-05-21 07:11:05 +0200

    Add containerization for the App

--- backend/utils/db_connector.py
+++ backend/utils/db_connector.py
@@ -1,7 +1,8 @@
 # backend/utils/db_connector.py
+import os
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient(os.getenv('MONGO_URL', 'mongodb://localhost:27017/'))
 db = client['skytrack_telemetry_db'] 
 
 def get_telemetry_collection():
